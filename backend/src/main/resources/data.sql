fd2338262b41ff421481232971b0aa60e4be4cde
Santiago Benavides <santibenavides.2005@gmail.com>
2026-05-09 19:35:26 -0500

    Uso de imagenes y configuración de vista de las mismas

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -17,10 +17,10 @@ INSERT INTO plataformas (nombre) VALUES ('PC'), ('PS5'), ('Xbox Series X'), ('Ni
 
 -- Insertar Videojuegos con Imágenes y Descuentos --
 INSERT INTO videojuegos (titulo, descripcion, stock, precio_base, descuento, imagen_url) VALUES 
-('Elden Ring', 'Explora las Tierras Intermedias en este RPG de acción épico.', 15, 59.99, 10, 'https://image.api.playstation.com/vulcan/ap/rnd/202110/2000/aajm89u9v6Y6XvXvXvXvXvXv.png'),
-('Cyberpunk 2077', 'Un RPG de acción y aventura de mundo abierto ambientado en Night City.', 20, 49.99, 20, 'https://image.api.playstation.com/vulcan/ap/rnd/202111/3013/itXvXvXvXvXvXvXvXv.png'),
-('FC 24', 'La nueva era del juego del mundo con licencias oficiales.', 50, 69.99, 0, 'https://image.api.playstation.com/vulcan/ap/rnd/202307/1012/6XvXvXvXvXvXvXv.png'),
-('Hollow Knight', 'Una aventura de acción clásica en 2D en un vasto mundo interconectado.', 35, 14.99, 0, 'https://assets.nintendo.com/image/upload/ar_16:9,c_lpad,w_1240/b_white/f_auto/q_auto/ncom/software/switch/70010000003208/Hollow-Knight-Switch-Hero');
+('Elden Ring', 'Explora las Tierras Intermedias en este RPG de acción épico.', 15, 59.99, 10, 'https://store-images.s-microsoft.com/image/apps.25322.14537704372270848.6ecb6038-5426-409a-8660-158d1eb64fb0.d230176a-d7a2-4696-ad23-ff53a6e004df'),
+('Cyberpunk 2077', 'Un RPG de acción y aventura de mundo abierto ambientado en Night City.', 20, 49.99, 20, 'https://store-images.s-microsoft.com/image/apps.47379.63407868131364914.bcaa868c-407e-42c2-baeb-48a3c9f29b54.89bb995b-b066-4a53-9fe4-0260ce07e894'),
+('FC 24', 'La nueva era del juego del mundo con licencias oficiales.', 50, 69.99, 0, 'https://store-images.s-microsoft.com/image/apps.62211.13743940396065041.b843213c-0751-4e6c-8c85-83412bf1f1aa.04161b63-31e2-4743-b77b-cdb3fb3dccd6'),
+('Hollow Knight', 'Una aventura de acción clásica en 2D en un vasto mundo interconectado.', 35, 14.99, 0, 'https://m.media-amazon.com/images/M/MV5BMGIyYmJmZDgtOWQ1Ny00NDFiLTk2OTgtM2Q2ZWQ4OWIxZjg3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg');
 
 -- Asociar Videojuegos con Categorías (vj_cat) --
 INSERT INTO vj_cat (id_vj, id_cat) VALUES 
